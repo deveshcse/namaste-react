6603ea96983614782825e80104a0153edc664b09
deveshcse <d.mishra56603@gmail.com>
2024-08-28 13:19:37 +0530

    episode1

---
+++ app.js
@@ -0,0 +1,38 @@
+/* <div id="parent">
+    <div id="child">
+        <h1 id="headingone"></h1>
+        <h2 id="headingtwo"></h2>
+    </div>
+        <div id="child2">
+        <h1 id="headingone"></h1>
+        <h2 id="headingtwo"></h2>
+    </div>
+</div> */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[
+    React.createElement(
+        "div",
+        { id: "childone" },
+        [
+            React.createElement("h1", { id: "headingone" }, "I am h1 tag"),
+            React.createElement("h2", { id: "headingtwo" }, "I am h2 tag")
+        ]),
+    React.createElement(
+        "div",
+        { id: "childtwo" },
+        [
+                React.createElement("h1", { id: "headingone" }, "I am h1 tag"),
+                React.createElement("h2", { id: "headingtwo" }, "I am h2 tag")
+        ])
+  ]
+  );
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world in react"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
