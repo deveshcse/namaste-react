cb7e1476d45fa5a1ff5cd626ad02742ced9366b7
deveshcse <d.mishra56603@gmail.com>
2024-09-02 20:50:40 +0530

    episode4

--- app.js
+++ app.js
@@ -1,106 +1,402 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-
-/* <div id="parent">
-    <div id="child">
-        <h1 id="headingone"></h1>
-        <h2 id="headingtwo"></h2>
-    </div>
-        <div id="child2">
-        <h1 id="headingone"></h1>
-        <h2 id="headingtwo"></h2>
-    </div>
-</div> */
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },[
-//     React.createElement(
-//         "div",
-//         { id: "childone" },
-//         [
-//             React.createElement("h1", { id: "headingone" }, "This is Namaste Raeact"),
-//             React.createElement("h2", { id: "headingtwo" }, "I am h2 tag")
-//         ]),
-//     React.createElement(
-//         "div",
-//         { id: "childtwo" },
-//         [
-//                 React.createElement("h1", { id: "headingone" }, "I am h1 tag"),
-//                 React.createElement("h2", { id: "headingtwo" }, "I am h2 tag")
-//         ])
-//   ]
-//   );
-
-// // const heading = React.createElement(
-// //   "h1",
-// //   { id: "heading" },
-// //   "hello world in react"
-// // );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-
-// Episode 3
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createelement => Object=> HTMLElement(render)
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - RestaurantCard
+ *          - img
+ *          - Name of res, star rating, cuisine, delivery time etc
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ *
+ */
 
-// const heading = React.createElement("h1", {id: "heading"}, "Namaste React........")
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading)
-
-// JSX - HTML like or XML like Syntax
-// JSX (transpiled before it reacjes the js) - PARCEL- Babel
-// JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-// const jsxHeading = (
-//   <h1 id="heading" className="head" tabIndex="1">
-//     Namaste React using JSX
-//   </h1>
-// );
-
-// console.log(jsxHeading);
-// // console.log(heading);
-// root.render(jsxHeading);
-
-const elem = <span>React Element........</span> 
-
-// React Element
-const Heading = (
-    <h1 id="heading" className="head" tabIndex="1">
-      {elem}
-      Namaste React using JSX
-    </h1>
-    
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
   );
+};
 
-// React component
-// 1. Class Based Components - OLD
-// 2. Functional Components - NEW
+const RestaurentCard = (props) => {
+  const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    avgRating,
+    cuisines,
+    costForTwo,
+    deliveryTime,
+  } = resData?.info;
 
-// React Functional Component
-const HeadingComponent1 = () =>(
-    <div id="container">
-    
-    <h1 className="head"> Namaste React Functional Component1</h1>
+  return (
+    <div className="restaurant-card">
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      ></img>
+      <h3>{name}</h3>
+      <h4>{avgRating} stars</h4>
+      <h4>{deliveryTime} mins</h4>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{costForTwo}</h4>
     </div>
-);
+  );
+};
 
-const HeadingComponent2 = () =>(
-    <div id="container">
-    <HeadingComponent1 />
-    {console.log("Can be used any javascript code in these braces")}
-    {Heading}
-    {HeadingComponent1()}
-    <h1> Namaste React Functional Component2</h1>
+const restaurantsList = [
+  {
+    info: {
+      id: "671928",
+      name: "KFC",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/510f05e2-a9e7-49fe-8ab3-ea8c2eb8a5ae_671928.JPG",
+      locality: "7th Block",
+      areaName: "Koramangla",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 4.3,
+      parentId: "547",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 1.2,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-09-03 02:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹50",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/kfc-7th-block-koramangla-rest671928",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "426776",
+      name: "Theobroma",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/22/059c1c9b-6e04-475a-8aba-1cba2e61953f_426776.jpg",
+      locality: "7th Block",
+      areaName: "Koramangala",
+      costForTwo: "₹400 for two",
+      cuisines: ["Desserts", "Bakery", "Beverages"],
+      avgRating: 4.7,
+      parentId: "1040",
+      avgRatingString: "4.7",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 2.4,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-09-02 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Desserts.png",
+            description: "Delivery!",
+          },
+          {
+            imageId: "newg.png",
+            description: "Gourmet",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Desserts.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "Gourmet",
+                  imageId: "newg.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "15% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/theobroma-7th-block-koramangala-rest426776",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "10576",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/87e56c63-b521-4257-96ae-a42229b92009_10576.jpg",
+      locality: "6th Block",
+      areaName: "Koramangala",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 0.9,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "0.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-09-03 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.4",
+          ratingCount: "1K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/pizza-hut-6th-block-koramangala-rest10576",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "5934",
+      name: "Burger King",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/7f76a072-c1bc-4d74-ac56-33e0eea20c1e_5934.JPG",
+      locality: "Koramangala",
+      areaName: "Koramangala",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 1.4,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "1.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-09-03 06:55:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹99",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.2",
+          ratingCount: "1K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/burger-king-koramangala-rest5934",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> Search </div>
+      <div className="restaurant-container">
+        {restaurantsList.map( (restaurant) =>(
+             <RestaurentCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-);
+  );
+};
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent2 />); 
+root.render(<AppLayout />);
